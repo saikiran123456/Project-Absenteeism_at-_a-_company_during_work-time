c01dc702f5b199e655be58a6373280bedfeee81d
Saikiran Dasari <37768258+saikiran123456@users.noreply.github.com>
2023-01-15 21:35:27 +0530

    Commit
    Added the Entire Pipeline of my Project Files

---
+++ 5MySQLintegration/DataGotInserted.sql
@@ -0,0 +1,36 @@
+# Analyzing the absenteeism predicted dataset (Python+SQL+Tableau Integration Project)
+
+DROP DATABASE IF EXISTS predicted_outputs;
+DROP DATABASE predicted_outputs;
+
+CREATE DATABASE IF NOT EXISTS predicted_outputs;
+
+USE predicted_outputs;
+
+SHOW tables;
+
+CREATE TABLE predicted_outputs
+(
+	reason_1 BIT NOT NULL,
+    reason_2 BIT NOT NULL,
+    reason_3 BIT NOT NULL,
+    reason_4 BIT NOT NULL,
+    month_value INT NOT NULL,
+    transportation_expense INT NOT NULL,
+    age INT NOT NULL,
+    body_mass_index INT NOT NULL,
+    education BIT NOT NULL,
+    children INT NOT NULL,
+    pets INT NOT NULL,
+    probability FLOAT NOT NULL,
+    prediction BIT NOT NULL
+);
+
+SELECT * FROM predicted_outputs;
+
+SELECT COUNT(*) FROM predicted_outputs; #40
+
+
+
+
+
